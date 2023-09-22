660769e338da4ae61fb453becca8e4f39dc2948e
Paul Wannenmacher <paul.wannenmacher@frachtwerk.de>
2023-09-22 10:50:28 +0200

    chore: add missing EqualsAndHashCode

--- essencium-backend/src/testIntegration/java/de/frachtwerk/essencium/backend/test/integration/model/dto/TestUserDto.java
+++ essencium-backend/src/testIntegration/java/de/frachtwerk/essencium/backend/test/integration/model/dto/TestUserDto.java
@@ -3,8 +3,10 @@ package de.frachtwerk.essencium.backend.test.integration.model.dto;
 import de.frachtwerk.essencium.backend.model.dto.UserDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.experimental.SuperBuilder;
 
+@EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @SuperBuilder(toBuilder = true)
 @Data
