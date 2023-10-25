cc9623ebd3321209524d654a702e34bf817bca22
Paul Wannenmacher <paul.wannenmacher@frachtwerk.de>
2023-10-25 13:14:36 +0200

    chore: add pre-create-check (unique constraint)

--- essencium-backend/src/main/java/de/frachtwerk/essencium/backend/repository/ApiTokenUserRepository.java
+++ essencium-backend/src/main/java/de/frachtwerk/essencium/backend/repository/ApiTokenUserRepository.java
@@ -4,7 +4,11 @@ import de.frachtwerk.essencium.backend.model.ApiTokenUser;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ApiTokenUserRepository extends JpaRepository<ApiTokenUser, UUID> {
+public interface ApiTokenUserRepository
+    extends JpaRepository<ApiTokenUser, UUID>, JpaSpecificationExecutor<ApiTokenUser> {
   List<ApiTokenUser> findByUser(String user);
+
+  boolean existsByUserAndDescription(String username, String description);
 }
