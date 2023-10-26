61d54b4e6f5fc87c65e8a0818d2a0514914aa75d
Paul Wannenmacher <paul.wannenmacher@frachtwerk.de>
2023-10-26 10:52:14 +0200

    chore: test controller methods

--- essencium-backend/src/main/java/de/frachtwerk/essencium/backend/controller/AbstractUserController.java
+++ essencium-backend/src/main/java/de/frachtwerk/essencium/backend/controller/AbstractUserController.java
@@ -286,7 +286,13 @@ public abstract class AbstractUserController<
                     .expiration(entity.getExpiration())
                     .userAgent(entity.getUserAgent())
                     .lastUsed(
-                        entity.getLastUsed().toInstant().atZone(ZoneOffset.UTC).toLocalDateTime())
+                        Objects.isNull(entity.getLastUsed())
+                            ? null
+                            : entity
+                                .getLastUsed()
+                                .toInstant()
+                                .atZone(ZoneOffset.UTC)
+                                .toLocalDateTime())
                     .build())
         .toList();
   }
