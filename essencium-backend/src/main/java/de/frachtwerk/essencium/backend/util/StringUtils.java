d1919e059b52d7dc5c9c01c89cb402755717d19d
Paul Wannenmacher <paul.wannenmacher@frachtwerk.de>
2023-10-30 15:28:35 +0100

    chore: test new regex

--- essencium-backend/src/main/java/de/frachtwerk/essencium/backend/util/StringUtils.java
+++ essencium-backend/src/main/java/de/frachtwerk/essencium/backend/util/StringUtils.java
@@ -39,11 +39,11 @@ public final class StringUtils {
     }
   }
 
-  // https://stackoverflow.com/a/16058059
   public static boolean isValidEmailAddress(String email) {
     String ePattern =
-        "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
-    Pattern p = java.util.regex.Pattern.compile(ePattern);
+        // "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+    Pattern p = Pattern.compile(ePattern);
     Matcher m = p.matcher(email);
     return m.matches();
   }
