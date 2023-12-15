0cc0ee4aeb7214c7059e5f00d94dc9805ecf1e42
Paul Wannenmacher <paul.wannenmacher@frachtwerk.de>
2023-12-15 13:17:08 +0100

    fix: UnauthorizedException now extends AuthenticationException givin back HTTP-Status 401

--- essencium-backend/src/main/java/de/frachtwerk/essencium/backend/model/exception/UnauthorizedException.java
+++ essencium-backend/src/main/java/de/frachtwerk/essencium/backend/model/exception/UnauthorizedException.java
@@ -20,10 +20,11 @@
 package de.frachtwerk.essencium.backend.model.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
-public class UnauthorizedException extends RuntimeException {
+public class UnauthorizedException extends AuthenticationException {
 
   public UnauthorizedException(final String message) {
     super(message);
