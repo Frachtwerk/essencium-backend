56417914ba0c361f6f5de943f1b05dd9d89f86fe
Paul Wannenmacher <paul.wannenmacher@frachtwerk.de>
2024-03-08 10:36:45 +0100

    chore: fmt

--- essencium-backend/src/main/java/de/frachtwerk/essencium/backend/security/oauth2/util/CookieUtil.java
+++ essencium-backend/src/main/java/de/frachtwerk/essencium/backend/security/oauth2/util/CookieUtil.java
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with essencium-backend. If not, see <http://www.gnu.org/licenses/>.
  */
+
 package de.frachtwerk.essencium.backend.security.oauth2.util;
 
 import jakarta.servlet.http.Cookie;
