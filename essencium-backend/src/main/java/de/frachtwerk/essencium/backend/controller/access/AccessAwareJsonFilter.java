9e279abe2b1105a563b968fcbc637483a5a2cd91
Paul Wannenmacher <paul.wannenmacher@frachtwerk.de>
2024-03-24 18:12:21 +0100

    chore: enable JsonAllowedFor for ApiTokenUser

--- essencium-backend/src/main/java/de/frachtwerk/essencium/backend/controller/access/AccessAwareJsonFilter.java
+++ essencium-backend/src/main/java/de/frachtwerk/essencium/backend/controller/access/AccessAwareJsonFilter.java
@@ -24,6 +24,7 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.PropertyWriter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import de.frachtwerk.essencium.backend.model.AbstractBaseUser;
+import de.frachtwerk.essencium.backend.model.ApiTokenUser;
 import de.frachtwerk.essencium.backend.model.Ownable;
 import de.frachtwerk.essencium.backend.model.Role;
 import java.io.Serializable;
@@ -31,38 +32,57 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @AllArgsConstructor
-public class AccessAwareJsonFilter<USER extends AbstractBaseUser<ID>, ID extends Serializable>
+public class AccessAwareJsonFilter<
+        U extends UserDetails, USER extends AbstractBaseUser<ID>, ID extends Serializable>
     extends SimpleBeanPropertyFilter {
-  private USER principal;
+  private U userDetails;
 
   @Override
   public void serializeAsField(
       Object pojo, JsonGenerator jsonGenerator, SerializerProvider provider, PropertyWriter writer)
       throws Exception {
     JsonAllowFor ann = writer.getMember().getAnnotation(JsonAllowFor.class);
-    if (include(writer)
-        && (ann == null
-            || Arrays.stream(ann.roles())
-                .anyMatch(s -> principal.getRoles().stream().map(Role::getName).anyMatch(s::equals))
-            || Stream.of(ann.rights())
-                .anyMatch(
-                    r ->
-                        principal.getAuthorities().stream()
-                            .map(GrantedAuthority::getAuthority)
-                            .anyMatch(r::equals))
-            || (ann.allowForOwner() && isOwner(pojo)))) {
-      writer.serializeAsField(pojo, jsonGenerator, provider);
-    } else if (!jsonGenerator.canOmitFields()) {
-      writer.serializeAsOmittedField(pojo, jsonGenerator, provider);
+    if (userDetails instanceof ApiTokenUser principal) {
+      if (include(writer)
+          && (ann == null
+              || Stream.of(ann.rights())
+                  .anyMatch(
+                      r ->
+                          principal.getAuthorities().stream()
+                              .map(GrantedAuthority::getAuthority)
+                              .anyMatch(r::equals)))) {
+        writer.serializeAsField(pojo, jsonGenerator, provider);
+      } else if (!jsonGenerator.canOmitFields()) {
+        writer.serializeAsOmittedField(pojo, jsonGenerator, provider);
+      }
+    }
+    if (userDetails instanceof AbstractBaseUser<?> principal) {
+      if (include(writer)
+          && (ann == null
+              || Arrays.stream(ann.roles())
+                  .anyMatch(
+                      s -> principal.getRoles().stream().map(Role::getName).anyMatch(s::equals))
+              || Stream.of(ann.rights())
+                  .anyMatch(
+                      r ->
+                          principal.getAuthorities().stream()
+                              .map(GrantedAuthority::getAuthority)
+                              .anyMatch(r::equals))
+              || (ann.allowForOwner() && isOwner(pojo)))) {
+        writer.serializeAsField(pojo, jsonGenerator, provider);
+      } else if (!jsonGenerator.canOmitFields()) {
+        writer.serializeAsOmittedField(pojo, jsonGenerator, provider);
+      }
     }
   }
 
   @SuppressWarnings("unchecked")
   private boolean isOwner(Object obj) {
     if (Ownable.class.isAssignableFrom(obj.getClass())) {
-      return ((Ownable<USER, ID>) obj).isOwnedBy(principal);
+      return ((Ownable<USER, ID>) obj).isOwnedBy((USER) userDetails);
     } else {
       return false;
     }
