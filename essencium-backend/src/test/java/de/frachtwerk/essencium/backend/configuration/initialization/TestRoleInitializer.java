a5f46edf054ae5fab89f410e7ac2d9c6856545e7
Paul Wannenmacher <paul.wannenmacher@frachtwerk.de>
2024-03-28 18:40:29 +0100

    fix: prevent an ADMIN-Role from not having all BasicApplicationRights

--- essencium-backend/src/test/java/de/frachtwerk/essencium/backend/configuration/initialization/TestRoleInitializer.java
+++ essencium-backend/src/test/java/de/frachtwerk/essencium/backend/configuration/initialization/TestRoleInitializer.java
@@ -6,10 +6,7 @@ import de.frachtwerk.essencium.backend.model.Role;
 import de.frachtwerk.essencium.backend.repository.RoleRepository;
 import de.frachtwerk.essencium.backend.service.RightService;
 import de.frachtwerk.essencium.backend.service.RoleService;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class TestRoleInitializer extends DefaultRoleInitializer {
 
@@ -27,15 +24,15 @@ public class TestRoleInitializer extends DefaultRoleInitializer {
 
   @Override
   protected Collection<Role> getAdditionalRoles() {
-    Right right = Right.builder().authority("right").build();
+    HashSet<Right> rights = new HashSet<>(List.of(Right.builder().authority("right").build()));
     return new ArrayList<>(
         List.of(
             Role.builder()
                 .name("TEST")
                 .description("TEST")
                 .isDefaultRole(defaultRole)
-                .rights(Set.of(right))
+                .rights(rights)
                 .build(),
-            Role.builder().name("USER").description("User").rights(Set.of(right)).build()));
+            Role.builder().name("USER").description("User").rights(rights).build()));
   }
 }
