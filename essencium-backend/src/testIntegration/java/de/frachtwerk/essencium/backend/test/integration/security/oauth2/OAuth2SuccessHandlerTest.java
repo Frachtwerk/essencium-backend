0ec60544bfb02ecd1a1efcd85b9d3f063d926d3b
Paul Wannenmacher <paul.wannenmacher@frachtwerk.de>
2024-08-27 09:03:12 +0200

    chore: fix integration tests

--- essencium-backend/src/testIntegration/java/de/frachtwerk/essencium/backend/test/integration/security/oauth2/OAuth2SuccessHandlerTest.java
+++ essencium-backend/src/testIntegration/java/de/frachtwerk/essencium/backend/test/integration/security/oauth2/OAuth2SuccessHandlerTest.java
@@ -138,12 +138,11 @@ class OAuth2SuccessHandlerTest {
 
     when(oAuth2ClientRegistrationPropertiesMock.getProvider()).thenReturn(testauth);
 
-    when(userServiceMock.loadUserByUsername(anyString()))
-        .thenThrow(new UsernameNotFoundException("e"));
+    when(userServiceMock.loadByUsername(anyString())).thenThrow(new UsernameNotFoundException("e"));
 
     oAuth2SuccessHandler.onAuthenticationSuccess(request, response, tokenMock);
 
-    verify(userServiceMock, times(1)).loadUserByUsername("admin.user@test.te");
+    verify(userServiceMock, times(1)).loadByUsername("admin.user@test.te");
 
     ArgumentCaptor<UserInfoEssentials> userInfoCaptor =
         ArgumentCaptor.forClass(UserInfoEssentials.class);
@@ -201,12 +200,11 @@ class OAuth2SuccessHandlerTest {
 
     when(oAuth2ClientRegistrationPropertiesMock.getProvider()).thenReturn(testauth);
 
-    when(userServiceMock.loadUserByUsername(anyString()))
-        .thenThrow(new UsernameNotFoundException("e"));
+    when(userServiceMock.loadByUsername(anyString())).thenThrow(new UsernameNotFoundException("e"));
 
     oAuth2SuccessHandler.onAuthenticationSuccess(request, response, tokenMock);
 
-    verify(userServiceMock, times(1)).loadUserByUsername("admin.user@test.te");
+    verify(userServiceMock, times(1)).loadByUsername("admin.user@test.te");
 
     ArgumentCaptor<UserInfoEssentials> userInfoCaptor =
         ArgumentCaptor.forClass(UserInfoEssentials.class);
@@ -261,12 +259,12 @@ class OAuth2SuccessHandlerTest {
 
     when(oAuth2ClientRegistrationPropertiesMock.getProvider()).thenReturn(testauth);
 
-    when(userServiceMock.loadUserByUsername("admin.user@test.te"))
+    when(userServiceMock.loadByUsername("admin.user@test.te"))
         .thenThrow(new UsernameNotFoundException("e"));
 
     oAuth2SuccessHandler.onAuthenticationSuccess(request, response, tokenMock);
 
-    verify(userServiceMock, times(1)).loadUserByUsername("admin.user@test.te");
+    verify(userServiceMock, times(1)).loadByUsername("admin.user@test.te");
 
     ArgumentCaptor<UserInfoEssentials> userInfoCaptor =
         ArgumentCaptor.forClass(UserInfoEssentials.class);
