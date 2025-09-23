b091d3a92dc46f3b2903f97a6dadc3c50b0746ed
Paul Wannenmacher <paul.wannenmacher@frachtwerk.de>
2025-09-23 12:17:52 +0200

    fix: enhance ID retrieval and additional claim casting in EssenciumUserDetails

--- essencium-backend/src/main/java/de/frachtwerk/essencium/backend/model/dto/EssenciumUserDetails.java
+++ essencium-backend/src/main/java/de/frachtwerk/essencium/backend/model/dto/EssenciumUserDetails.java
@@ -67,7 +67,78 @@ public class EssenciumUserDetails<ID extends Serializable> implements UserDetail
     return Objects.requireNonNullElse(Locale.forLanguageTag(locale), DEFAULT_LOCALE);
   }
 
+  public ID getId() {
+    return switch (id) {
+      case Long l -> (ID) l;
+      case Integer i -> (ID) Long.valueOf(i.longValue());
+      default -> id;
+    };
+  }
+
   public Object getAdditionalClaimByKey(String key) {
     return additionalClaims.get(key);
+  }
+
+  public <O> O getAdditionalClaimByKey(String key, Class<O> clazz) {
+    Object object = getAdditionalClaims().get(key);
+    if (object != null && clazz.isAssignableFrom(object.getClass())) {
+      return clazz.cast(object);
+    } else if (object == null) {
+      return null;
+    }
+
+    if (clazz == Long.class) {
+      switch (object) {
+        case Number number -> {
+          return clazz.cast(number.longValue());
+        }
+        case String str -> {
+          try {
+            return clazz.cast(Long.valueOf(str));
+          } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Cannot convert String to Long: " + str, e);
+          }
+        }
+        case Boolean bool -> {
+          return clazz.cast(bool ? 1L : 0L);
+        }
+        default -> {}
+      }
+    } else if (clazz == Integer.class) {
+      switch (object) {
+        case Number number -> {
+          return clazz.cast(number.intValue());
+        }
+        case String str -> {
+          try {
+            return clazz.cast(Integer.valueOf(str));
+          } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Cannot convert String to Integer: " + str, e);
+          }
+        }
+        case Boolean bool -> {
+          return clazz.cast(bool ? 1 : 0);
+        }
+        default -> {}
+      }
+    } else if (clazz == String.class) {
+      return clazz.cast(object.toString());
+    } else if (clazz == Boolean.class) {
+      switch (object) {
+        case Boolean bool -> {
+          return clazz.cast(bool);
+        }
+        case String str -> {
+          return clazz.cast(Boolean.valueOf(str));
+        }
+        case Number number -> {
+          return clazz.cast(number.intValue() != 0);
+        }
+        default -> {}
+      }
+    } else {
+      return clazz.cast(object);
+    }
+    return (O) object;
   }
 }
