11d232fdb06cdf1119d820837882781fb26c9cf7
Paul Wannenmacher <paul.wannenmacher@frachtwerk.de>
2025-10-23 12:13:23 +0200

    feat: add copyright notice

--- essencium-backend/src/testIntegration/java/de/frachtwerk/essencium/backend/test/integration/configuration/UserTokenInvalidationAspectTest.java
+++ essencium-backend/src/testIntegration/java/de/frachtwerk/essencium/backend/test/integration/configuration/UserTokenInvalidationAspectTest.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2025 Frachtwerk GmbH, Leopoldstraße 7C, 76133 Karlsruhe.
+ *
+ * This file is part of essencium-backend.
+ *
+ * essencium-backend is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * essencium-backend is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with essencium-backend. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.frachtwerk.essencium.backend.test.integration.configuration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,7 +30,6 @@ import de.frachtwerk.essencium.backend.test.integration.IntegrationTestApplicati
 import de.frachtwerk.essencium.backend.test.integration.model.TestUser;
 import de.frachtwerk.essencium.backend.test.integration.repository.TestBaseUserRepository;
 import de.frachtwerk.essencium.backend.test.integration.util.TestingUtils;
-
 import java.time.Duration;
 import java.util.List;
 import java.util.Objects;
