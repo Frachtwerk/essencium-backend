d05a6317e8f707c422dfec7c3935cfc87dbf5e8f
Paul Wannenmacher <paul.wannenmacher@frachtwerk.de>
2026-06-11 15:02:06 +0200

    feat: add JsonAllowForController and payload for role-based visibility

---
+++ essencium-backend/src/testIntegration/java/de/frachtwerk/essencium/backend/test/integration/app/controller/JsonAllowForController.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2026 Frachtwerk GmbH, Leopoldstraße 7C, 76133 Karlsruhe.
+ *
+ * This file is part of essencium-backend.
+ *
+ * essencium-backend is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * essencium-backend is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with essencium-backend. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.frachtwerk.essencium.backend.test.integration.app.controller;
+
+import de.frachtwerk.essencium.backend.test.integration.app.model.dto.JsonAllowForPayload;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/native")
+public class JsonAllowForController {
+
+  @Secured({"ADMIN", "Test"})
+  @GetMapping("/json-allow-for")
+  public JsonAllowForPayload getJsonAllowForPayload() {
+    return new JsonAllowForPayload("always-visible", "admin-visible");
+  }
+}
