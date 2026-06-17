e09e04609f88265a25670e09785bc3a6a131a127
Andrii Udodenko <andrii.udodenko@frachtwerk.de>
2026-06-17 14:56:40 +0200

    refactor: remove ResponseStatus from domain exceptions

--- essencium-backend/src/main/java/de/frachtwerk/essencium/backend/model/exception/TokenInvalidationException.java
+++ essencium-backend/src/main/java/de/frachtwerk/essencium/backend/model/exception/TokenInvalidationException.java
@@ -19,10 +19,6 @@
 
 package de.frachtwerk.essencium.backend.model.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class TokenInvalidationException extends RuntimeException {
 
   public TokenInvalidationException() {
