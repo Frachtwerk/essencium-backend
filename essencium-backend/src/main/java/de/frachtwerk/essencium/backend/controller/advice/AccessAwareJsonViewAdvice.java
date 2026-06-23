95fc95f1f68f11bee8fdef559d1aa15af0c88f3b
Paul Wannenmacher <paul.wannenmacher@frachtwerk.de>
2026-06-23 10:57:10 +0200

    chore: remove unused import for SecurityContextHolder in AccessAwareJsonViewAdvice

--- essencium-backend/src/main/java/de/frachtwerk/essencium/backend/controller/advice/AccessAwareJsonViewAdvice.java
+++ essencium-backend/src/main/java/de/frachtwerk/essencium/backend/controller/advice/AccessAwareJsonViewAdvice.java
@@ -31,7 +31,6 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.JacksonJsonHttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 import tools.jackson.databind.ser.FilterProvider;
