1a5667065b8fde41827fc54f15e5ec7212ae2d12
Andrii Udodenko <andrii.udodenko@frachtwerk.de>
2026-06-30 12:43:33 +0200

    fix: apply formatting to error handling files

--- essencium-backend/src/main/java/de/frachtwerk/essencium/backend/controller/advice/ErrorCode.java
+++ essencium-backend/src/main/java/de/frachtwerk/essencium/backend/controller/advice/ErrorCode.java
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with essencium-backend. If not, see <http://www.gnu.org/licenses/>.
  */
+
 package de.frachtwerk.essencium.backend.controller.advice;
 
 public enum ErrorCode {
