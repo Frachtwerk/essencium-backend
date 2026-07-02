2c7b918d598751cd9d14be715c8b1f0c3031b359
Paul Wannenmacher <paul.wannenmacher@frachtwerk.de>
2026-07-02 15:51:51 +0200

    feat: add security scheme for JWT authentication in OpenAPI definition

--- essencium-backend-development-uuid/src/main/java/de/frachtwerk/essencium/backend/SpringBootApp.java
+++ essencium-backend-development-uuid/src/main/java/de/frachtwerk/essencium/backend/SpringBootApp.java
@@ -22,6 +22,10 @@ package de.frachtwerk.essencium.backend;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -35,8 +39,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ConfigurationPropertiesScan(basePackages = {"de.frachtwerk.essencium.backend"})
 @EnableJpaRepositories(basePackages = {"de.frachtwerk.essencium.backend"})
 @EnableAspectJAutoProxy
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT",
+    in = SecuritySchemeIn.HEADER)
 @OpenAPIDefinition(
     servers = {@Server(url = "${app.url:localhost:8098}")},
+    security = {@SecurityRequirement(name = "bearerAuth")},
     info =
         @Info(
             title = "Essencium API (UUID Development)",
